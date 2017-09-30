44cdfca92f0fd2eb260163cbc7fff494eb717876
Gabor Zoltan Toth <latotty@codingsans.com>
2017-09-30 17:26:00 +0200

    feat(key-modifiers): stream for modifiers

--- src/lib/key-modifiers.ts
+++ src/lib/key-modifiers.ts
@@ -1,3 +1,30 @@
+import xs from 'xstream';
+import fromEvent from 'xstream/extra/fromEvent';
+import dropRepeats from 'xstream/extra/dropRepeats';
+
+const keydownEvent$: xs<KeyboardEvent> = fromEvent(document, 'keydown');
+const keyupEvent$: xs<KeyboardEvent> = fromEvent(document, 'keyup');
+
+const keyEvents$ = xs.merge(keydownEvent$, keyupEvent$);
+
+const keyModifiers$ = keyEvents$.map(ev => ({
+  alt: ev.altKey,
+  ctrl: ev.ctrlKey,
+  shift: ev.shiftKey,
+}));
+
+export const altKey$ = keyModifiers$.map(mods => mods.alt)
+  .compose(dropRepeats())
+  .remember();
+
+export const ctrlKey$ = keyModifiers$.map(mods => mods.ctrl)
+  .compose(dropRepeats())
+  .remember();
+
+export const shiftKey$ = keyModifiers$.map(mods => mods.shift)
+  .compose(dropRepeats())
+  .remember();
+
 import { addCleanup } from './cleanup';
 export function initKeyListener() { // legacy
   const keysObj = {
