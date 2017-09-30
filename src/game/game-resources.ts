240171ba3c87f8d0748225e77f12661014e5b935
Gabor Zoltan Toth <latotty@codingsans.com>
2017-09-30 17:52:03 +0200

    feat(game-resource): resource streams

---
+++ src/game/game-resources.ts
@@ -0,0 +1,50 @@
+import xs from 'xstream';
+import dropRepeats from 'xstream/extra/dropRepeats';
+
+import { getResource, getProduction, getResourceEmc, getStorage, resourceTypes } from './api';
+import { ResourceType, AllResources } from './game.interface';
+
+interface ResourceStreams {
+  amount$: xs<number>;
+  production$: xs<number>;
+  emc$: xs<number>;
+  storage$: xs<number>;
+}
+
+const timer$ = xs.periodic(25);
+
+export const resources: AllResources<ResourceStreams> = resourceTypes.reduce<AllResources<ResourceStreams>>((state, type) => {
+  return {
+    ...state,
+    [type]: getResourceStream(type, timer$),
+  };
+}, {} as any)
+
+function getResourceStream(type: ResourceType, trigger$ = xs.periodic(25)) {
+  const amount$ = trigger$
+    .map(() => getResource(type))
+    .compose(dropRepeats())
+    .remember();
+
+  const production$ = trigger$
+    .map(() => getProduction(type))
+    .compose(dropRepeats())
+    .remember();
+
+  const emc$ = trigger$
+    .map(() => getResourceEmc(type))
+    .compose(dropRepeats())
+    .remember();
+
+  const storage$ = trigger$
+    .map(() => getStorage(type))
+    .compose(dropRepeats())
+    .remember();
+
+  return {
+    amount$,
+    production$,
+    emc$,
+    storage$
+  };
+}
